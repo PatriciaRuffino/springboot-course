c3802d8696def8f7361fce4eb8511607c4aab01c
PatriciaRuffino <sindelformr@gmail.com>
2022-08-11 12:03:29 -0300

    Project created

---
+++ src/test/java/com/ruffino/coursespringboot/CourseSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruffino.coursespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
